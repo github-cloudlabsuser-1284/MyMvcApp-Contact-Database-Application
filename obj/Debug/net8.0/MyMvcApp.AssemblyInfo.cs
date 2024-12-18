a91e95cb8b62e71155cc92a090f573752ef986fa
Sandeep Yadav <github_cloudlabsuser_1284@cloudlabsaiuser.com>
2024-12-18 08:46:16 +0000

    azure deploy

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd82fefd72712290c0236169d60b97888ec0833d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b49d31a7481e9350c60976fcdc0f99cdc39f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
